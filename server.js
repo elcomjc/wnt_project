7717066479e4ba19fbb0661261413ea290ade110
Juan Carlos Espinoza <elcom.jc@gmail.com>
2017-02-18 18:27:11 -0300

    Added port env variable

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(8080, 'localhost', function (err, result) {
+}).listen(process.env.PORT || 5000, 'localhost', function (err, result) {
   if (err) {
     console.log(err);
   }
 
-  console.log('Listening at localhost:8080');
+  console.log('Listening at localhost:'+process.env.PORT);
 });
